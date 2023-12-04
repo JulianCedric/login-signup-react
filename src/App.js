5f9677753f26c3cbbaf5d2899edbc3ceb0916495
Julian Pormentilla <julian.pormentilla@gmail.com>
2023-12-04 18:15:51 -0500

    Modified handleLogin function to accept event parameter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import './App.css';
 import React, { useState } from 'react';
 import Login from './Login';
+import { Button } from 'semantic-ui-react';
 
-function App() {
-  // const [ users, setUsers ] = useState(null);
+const USERS = [
+  {
+    id: 1,
+    username: 'user1',
+    password: 'pass1'
+  }
+];
+
+const App = () => {
+  const [users, setUsers] = useState(USERS);
+
+  const handleInitialize = message => {
+    console.log('[ TEST ] message:', message);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('Login event:', e);
+    console.log('e.target[0].value:', e.target[0].value);
+
+    if (e.target[0].value == 'user1' && e.target[1].value == 'pass1') {
+      console.log('user logged in');
+    } else {
+      alert('The username or password you entered is incorrect. Please try again.');
+      e.target[0].value = '';
+      e.target[1].value = '';
+      e.target[0].focus();
+    };
+  };
+
   return (
     <div className="App">
-      <Login />
+      <Login handleLogin={handleLogin} />
+      <Button 
+        color='blue'
+        onClick={() => handleInitialize('Login rendered.')}
+      >
+        Initialize
+      </Button>
+      <div>
+        <h1>here</h1>
+      </div>
     </div>
   );
 };
--- src/Login.js
+++ src/Login.js
@@ -1,61 +1,54 @@
 import React, { useState } from 'react';
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-
-    console.log('username:', username);
-    console.log('password:', password);
-
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-
-        console.log('submitted');
+        handleLogin(e);
     };
 
     return (
-        <>
         <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
             <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as='h1' color='blue' textAlign='center'>
-                Login
-            </Header>
-            <Form size='large' onSubmit={handleSubmit}>
-                <Segment stacked>
-                <Form.Input 
-                    fluid 
-                    icon='user' 
-                    iconPosition='left' 
-                    placeholder='Username' 
-                    type='text'
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <Form.Input
-                    fluid
-                    icon='lock'
-                    iconPosition='left'
-                    placeholder='Password'
-                    type='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-
-                <Button color='blue' fluid size='large'>
+                <Header as='h1' color='blue' textAlign='center'>
                     Login
-                </Button>
-                </Segment>
-            </Form>
-            <Message>
-                Don't have an account? <a href='#'>Sign up</a>
-            </Message>
+                </Header>
+                <Form size='large' onSubmit={handleSubmit}>
+                    <Segment stacked>
+                        <Form.Input 
+                            fluid 
+                            icon='user' 
+                            iconPosition='left' 
+                            placeholder='Username' 
+                            type='text'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                        <Form.Input
+                            fluid
+                            icon='lock'
+                            iconPosition='left'
+                            placeholder='Password'
+                            type='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+
+                        <Button color='blue' fluid size='large'>
+                            Login
+                        </Button>
+                    </Segment>
+                </Form>
+                <Message>
+                    Don't have an account? <a href='#'>Sign up</a>
+                </Message>
             </Grid.Column>
         </Grid>
-        </>
     );
 };
 
