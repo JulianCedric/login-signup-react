6156f9a6ba977f92134a0ef7e55cbd38cb708641
Julian Pormentilla <julian.pormentilla@gmail.com>
2023-12-03 23:57:44 -0500

    Updated Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,13 @@
 import { React } from 'react';
 
-// export default ;
+class Login extends React.Component {
+     render() {
+        return (
+            <div>
+                <p>Hello there!</p>
+            </div>
+        );
+     };
+};
+
+export default Login;
