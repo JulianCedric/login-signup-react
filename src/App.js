2f0599e79824dd3fe554e7dee1cfea4bbc8be872
Julian Pormentilla <julian.pormentilla@gmail.com>
2023-12-04 16:26:31 -0500

    Add semantic ui

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import Login from './Login';
 
 function App() {
-  const [ users, setUsers ] = useState(null);
+  // const [ users, setUsers ] = useState(null);
   return (
     <div className="App">
       <Login />
     </div>
   );
-}
+};
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,46 +1,61 @@
 import React, { useState } from 'react';
+import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 
-function Login() {
+const Login = () => {
+
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
 
     console.log('username:', username);
     console.log('password:', password);
 
-    const handleSubmit = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+
         console.log('submitted');
-    }
+    };
 
     return (
-        <form>
-            <h2>Login</h2>
-            <div>
-                <label>
-                    <input
-                        type='text'
-                        value={username}
-                        placeholder='Username'
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input
-                        type='text'
-                        value={password}
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-            </div>
-            <button
-                type='submit'
-                onSubmit={(e) => handleSubmit()}
-            >
-                Log in
-            </button>
-        </form>
+        <>
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h1' color='blue' textAlign='center'>
+                Login
+            </Header>
+            <Form size='large' onSubmit={handleSubmit}>
+                <Segment stacked>
+                <Form.Input 
+                    fluid 
+                    icon='user' 
+                    iconPosition='left' 
+                    placeholder='Username' 
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <Form.Input
+                    fluid
+                    icon='lock'
+                    iconPosition='left'
+                    placeholder='Password'
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <Button color='blue' fluid size='large'>
+                    Login
+                </Button>
+                </Segment>
+            </Form>
+            <Message>
+                Don't have an account? <a href='#'>Sign up</a>
+            </Message>
+            </Grid.Column>
+        </Grid>
+        </>
     );
 };
 
