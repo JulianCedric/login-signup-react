a0b670f903b63c1d7c5c25b92dc8861a96683e96
Julian Pormentilla <julian.pormentilla@gmail.com>
2023-12-06 00:45:59 -0500

    Updates to Clock.js - 1

--- src/Clock.js
+++ src/Clock.js
@@ -56,20 +56,17 @@ function Clock() {
       <Grid.Row>
         <Grid.Column>
           <Segment>
-            <p>Time Container 1 Dos:</p>
+            <p>Time Container 1 of 7 Dos:</p>
             <Card>
               <Card.Content>
-                <Image
-                  floated='right'
-                  size='mini'
-                  src='/images/avatar/large/steve.jpg'
-                />
                 <Card.Header>Time Container Title</Card.Header>
                 <Card.Meta>Time Container Description</Card.Meta>
                 <Card.Description>
-                  <Checkbox label='task 1 description'/>
-                  <Checkbox label='task 2 description'/>
-                  <Checkbox label='etc.'/>
+                  <List>
+                    <List.Item><Checkbox label='task 1 description'/></List.Item>
+                    <List.Item><Checkbox label='task 2 description'/></List.Item>
+                    <List.Item><Checkbox label='etc.'/></List.Item>
+                  </List>
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
@@ -86,20 +83,50 @@ function Clock() {
           </Segment>
         </Grid.Column>
         <Grid.Column>
-          <Segment>Time Container 1 Don'ts:</Segment>
+          <Segment>Time Container 1 of 7 Don'ts:</Segment>
         </Grid.Column>
       </Grid.Row>
       <Grid.Row>
         <Grid.Column>
           <Segment>
-            <p>It's time to do your Daily Exercise Routine, Julian.</p>
+            <p>Time Container 1 of 7 Dos:</p>
             <Card>
               <Card.Content>
-                <Image
-                  floated='right'
-                  size='mini'
-                  src='/images/avatar/large/steve.jpg'
-                />
+                <Card.Header>It's time to do your Daily Exercise Routine, Julian.</Card.Header>
+                <Card.Meta>This is your Keystone Habit. Doing it completely and correctly will cause an overall improvement to your current state. Approximate duration: 1.5 hours. Recurrence: Daily, Morning.</Card.Meta>
+                <Card.Description>
+                  <List>
+                    <List.Item><Checkbox label='Put on running gear.'/></List.Item>
+                    <List.Item><Checkbox label='Walk to gym.'/></List.Item>
+                    <List.Item><Checkbox label='Treadmill for at least 2 miles.'/></List.Item>
+                    <List.Item><Checkbox label='Sauna for at least 20 minutes.'/></List.Item>
+                    <List.Item><Checkbox label='Shower and get dressed for the day'/></List.Item>
+                  </List>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>
+                    Approve
+                  </Button>
+                  <Button basic color='red'>
+                    Decline
+                  </Button>
+                </div>
+              </Card.Content>
+            </Card>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>Time Container 1 of 7 Don'ts:</Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <Segment>
+            <p>Time Container 1 of 7:</p>
+            <Card>
+              <Card.Content>
                 <Card.Header>It's time to do your Daily Exercise Routine, Julian.</Card.Header>
                 <Card.Meta>This is your Keystone Habit. Doing it completely and correctly will cause an overall improvement to your current state. Approximate duration: 1 hour. Recurrence: Daily, Morning.</Card.Meta>
                 <Card.Description>
@@ -124,7 +151,7 @@ function Clock() {
           </Segment>
         </Grid.Column>
         <Grid.Column>
-          <Segment>Content</Segment>
+          <Segment>Time Container 1 of 7 ASSIST:</Segment>
         </Grid.Column>
       </Grid.Row>
     </Grid>
